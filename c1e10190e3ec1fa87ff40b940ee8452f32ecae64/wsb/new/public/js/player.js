868c9ff5c7204ea53e24b1c8d618dd68ae03f323
Julian Thompson <julianthompson@Julians-Mac-mini.local>
2022-11-05 15:45:10 +0000

    _

--- c1e10190e3ec1fa87ff40b940ee8452f32ecae64/wsb/new/public/js/player.js
+++ c1e10190e3ec1fa87ff40b940ee8452f32ecae64/wsb/new/public/js/player.js
@@ -25,8 +25,8 @@ Amplitude.init ({
             "artist": "[we sell boxes]",
             "id": "wsb015",
             "bcid": "3568645090",
-            "bclink": "g-ants-garden",
-            "url": "src/01 - The 8th Shade.mp3",
+            "bclink": "https://wesellboxes.bandcamp.com/track/g-ants-garden",
+            "url": "src/files/01.mp3",
             "cover_art_url": "https://static1.squarespace.com/static/5608382ae4b017614f857528/t/5a67824c8165f553a1c85ac9/1516733005222/Colorful-Abstract-Art-by-Thaneeya-McArdle.jpg"
         },
         {
@@ -35,7 +35,7 @@ Amplitude.init ({
             "id": "wsb014",
             "bcid": "3481653095",
             "bclink": "grief-vultures",
-            "url": "src/02 - Theme From Mish Mash.mp3",
+            "url": "src/files/02.mp3",
             "cover_art_url": "https://i.pinimg.com/564x/95/ef/75/95ef75f29d3f3546fff036eb53441c34.jpg"
         },
         {
@@ -44,7 +44,7 @@ Amplitude.init ({
             "id": "wsb013",
             "bcid": "2424135838",
             "bclink": "untitled-electro-track-1",
-            "url": "src/03 - Now I Deliver.mp3",
+            "url": "src/files/03.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2018/02/we_sell_boxes-untitled_electro_track_1-500x500.png"
         },
         {
@@ -53,7 +53,7 @@ Amplitude.init ({
             "id": "wsb012",
             "bcid": "1715679061",
             "bclink": "flat-on-my-back",
-            "url": "src/04 - Uneasy Skanking.mp3",
+            "url": "src/files/04.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2018/01/we_sell_boxes-flat_on_my_back-500x500.png"
         },
         {
@@ -62,7 +62,7 @@ Amplitude.init ({
             "id": "wsb011",
             "bcid": "2934462759",
             "bclink": "carpetbaggers",
-            "url": "src/05 - Fling Mi Ting.mp3",
+            "url": "src/files/05.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2017/07/we_sell_boxes-carpetbaggers-500x500.jpg"
         },
         {
@@ -71,7 +71,7 @@ Amplitude.init ({
             "id": "wsb010",
             "bcid": "60008476",
             "bclink": "seperate-rooms",
-            "url": "src/06 - Mish Mash Episode One.mp3",
+            "url": "src/files/06.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2018/01/we_sell_boxes-acluophobia.jpg"
         },
         {
@@ -80,7 +80,7 @@ Amplitude.init ({
             "id": "wsb009",
             "bcid": "2596123303",
             "bclink": "comments-section",
-            "url": "src/07 - 90° Fuzzwalk.mp3",
+            "url": "src/files/07.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2017/02/we_sell_boxes-comments_section-1400-500x500.jpg"
         },
         {
@@ -89,7 +89,7 @@ Amplitude.init ({
             "id": "wsb008",
             "bcid": "1510990226",
             "bclink": "walk-you-through-this",
-            "url": "src/08 - Mish Mash Episode Two.mp3",
+            "url": "src/files/08.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2016/05/we_sell_boxes-walk_you_through_this-800-500x500.jpg"
         },
         {
@@ -98,7 +98,7 @@ Amplitude.init ({
             "id": "wsb007",
             "bcid": "3152065329",
             "bclink": "foolish-proles-make-it-easy",
-            "url": "src/09 - Going Under.mp3",
+            "url": "src/files/09.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2016/04/we_sell_boxes-foolish_proles-1400-500x500.jpg"
         },
         {
@@ -107,7 +107,7 @@ Amplitude.init ({
             "id": "wsb006",
             "bcid": "2177854771",
             "bclink": "mllody",
-            "url": "src/10 - Paths Of Life.mp3",
+            "url": "src/files/10.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2016/04/we_sell_boxes-mllody-1400-500x500.jpg"
         },
         {
@@ -116,7 +116,7 @@ Amplitude.init ({
             "id": "wsb005",
             "bcid": "2282986145",
             "bclink": "owed50",
-            "url": "src/11 - One With Another",
+            "url": "src/files/11.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2016/04/we_sell_boxes-owed50-1400-500x500.jpg"
         },
         {
@@ -125,7 +125,7 @@ Amplitude.init ({
             "id": "wsb004",
             "bcid": "502188643",
             "bclink": "divisive-strategies",
-            "url": "src/12 - Mish Mash Episode Three.mp3",
+            "url": "src/files/01.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2016/04/we_sell_boxes-divisive_strategies-1400-500x500.jpg"
         },
         {
@@ -134,7 +134,7 @@ Amplitude.init ({
             "id": "wsb003",
             "bcid": "3568135637",
             "bclink": "creating-prosperity",
-            "url": "src/12 - Mish Mash Episode Three.mp3",
+            "url": "src/files/02.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2016/04/we_sell_boxes-creating_prosperity-1400-500x500.jpg"
         },
         {
@@ -143,7 +143,7 @@ Amplitude.init ({
             "id": "wsb002",
             "bcid": "3371817904",
             "bclink": "helpless",
-            "url": "src/12 - Mish Mash Episode Three.mp3",
+            "url": "src/files/03.mp3",
             "cover_art_url": "https://www.wesellboxes.net/wp-content/uploads/2016/04/we_sell_boxes-helpless-500.jpg"
         },
         {
@@ -152,7 +152,7 @@ Amplitude.init ({
             "id": "wsb002",
             "bcid": "1588190798",
             "bclink": "bbd-sc",
-            "url": "src/12 - Mish Mash Episode Three.mp3",
+            "url": "src/files/04.mp3",
             "cover_art_url": "https://i.scdn.co/image/ab67616d0000b27382fcb5ea65b6398ba187ad01"
         }
     ]
--- c1e10190e3ec1fa87ff40b940ee8452f32ecae64/wsb/new/tailwind.config.js
+++ c1e10190e3ec1fa87ff40b940ee8452f32ecae64/wsb/new/tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     './public/**/*.{html,js}'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'courier': ['Courier New', 'Courier', 'monospace'],
+    },
+    extend: {
+      backgroundImage: {
+        'video-poster': "url('https://wesellboxes.net/we_sell_boxes-video-screenshot.jpg')",
+        'hands': "url('../src/images/hands.jpeg')",
+        'bg1': "url('../src/images/studio.jpg')",
+        'bg2': "url('../src/images/space.jpg')",
+        'bg3': "url('../src/images/warehouse.jpg')",
+        'bg4': "url('../src/images/warehouse2.jpg')",
+        'bg5': "url('../src/images/odyssey-1979.jpg')",
+        'bc-logo': "url('https://upload.wikimedia.org/wikipedia/commons/8/80/Bandcamp-logotype-color.svg')",
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
